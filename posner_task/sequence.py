9cd06f1ebac53cf6b0f70abe0ee4731300b52bd0
Ole Bialas <ole.bialas@posteo.de>
2024-09-19 15:30:24 +0200

    Add sequence class

---
+++ posner_task/sequence.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import collections
+import csv
+import numpy as np
+
+
+class Block(collections.abc.Iterator):
+    def __init__(self, n_trials, p_valid, min_dist=3, max_iter=1000):
+        self.side, self.valid = self.make_trial_sequence(
+            n_trials, p_valid, min_dist, max_iter
+        )
+        self.n_trials = n_trials
+        self.this_n = 0
+        self.response = [None] * n_trials
+        self.response_time = [None] * n_trials
+
+    def __next__(self):
+        self.this_n += 1
+        if self.this_n == self.n_trials:
+            raise StopIteration
+        else:
+            return self.side[self.this_n], self.valid[self.this_n]
+
+    @staticmethod
+    def make_trial_sequence(n_trials, p_valid, min_dist=3, max_iter=1000):
+        side, valid = [], []
+        if n_trials / 2 * p_valid % 1 != 0:
+            raise ValueError("Trials can't be evenly divided between conditions!")
+
+        for s in ["left", "right"]:
+            n = int(n_trials / 2)
+            side += [s] * n
+            n_valid = int(n * p_valid)
+            valid += [True] * n_valid + [False] * (n - n_valid)
+
+        ok, count = 0, 0
+        while not ok:
+            idx = np.random.choice(n_trials, n_trials, replace=False)
+            side = np.asarray(side)[idx]
+            valid = np.asarray(valid)[idx]
+            dist = np.diff(np.where(valid == False)[0])
+            if dist.min() >= min_dist:
+                ok = 1
+            count += 1
+            if count >= max_iter:
+                raise ValueError(f"Couldn't find sequence after {max_iter} iterations!")
+        return side, valid
+
+    def add_response(self, response, response_time):
+        self.response[self.this_n] = response
+        self.response_time[self.this_n] = response
+
+    def save(self, path, mkdir=False):
+        path = Path(path)
+        if not path.suffix == ".csv":
+            path = path.parent / path.name + ".csv"
+        if not path.parent.exists():
+            if mkdir:
+                path.parent.mkdir(parents=True)
+            else:
+                raise ValueError(f"Folder {path.parent} does not exist!")
+        rows = zip(self.side, self.valid, self.response, self.response_time)
+        with open(path, "w", newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(["side", "valid", "response", "time"])  # header
+            csvwriter.writerows(rows)  # data
